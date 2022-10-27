a1c9d113addfdb9ea762011e413383a191664dff
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-10-27 12:57:44 +0100

    sweet

--- Program.cs
+++ Program.cs
@@ -2,6 +2,21 @@ using Microsoft.Extensions.FileProviders;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    // if the route doesn't have a file extension return the index.html
+    if (!context.Request.Path.Value!.Contains("."))
+    {
+        var indexFile = File.ReadAllText("./client/dist/index.html");
+        await context.Response.WriteAsync(indexFile);
+    }
+    else
+    {
+        await next();
+    }
+}
+ );
+
 app.UseStaticFiles(
     new StaticFileOptions
     {
